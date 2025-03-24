e094e88f7213ff51b1e8a4ac2adfa9b19fdda80f
rohan-zero <rohankanagala2@gmail.com>
2025-03-24 23:50:15 +0530

    Structured components according to blueprint

--- Documents/projects/Zerodha/frontend/src/index.js
+++ Documents/projects/Zerodha/frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import HomePage from "./landing_page/home/HomePage";
+import AboutPage from "./landing_page/about/AboutPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <HomePage />
+    <AboutPage />
   </React.StrictMode>
 );
