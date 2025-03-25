feebb28fa81c1d344aeacb118660adb6b007e8eb
rohan-zero <rohankanagala2@gmail.com>
2025-03-25 23:55:59 +0530

    Added images and fonts. Started workign on home page

--- Documents/projects/Zerodha/frontend/src/landing_page/home/Awards.js
+++ Documents/projects/Zerodha/frontend/src/landing_page/home/Awards.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+        <div className='container p-5'>
+            <div className='row'>
+                <div className='col-6 p-5'>
+                    <img src='media/images/largestBroker.svg'/>
+                </div>
+                <div className='col-6'>
+                    <h1>Largest stock broker in India</h1>
+                    <p>2+ million Zerodha clients contribute to over 15% of all retail
+                    order volumes in India daily by trading and investing in:</p>
+                    
+                </div>
+            </div>
+        </div>
      );
 }
 
